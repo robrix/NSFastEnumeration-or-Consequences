f1a4c108b81771607e66dc8dc6ee0908165600f1
Rob Rix <rob.rix@github.com>
2013-11-12 12:20:57 -0500

    Add an Xcode project.

---
+++ NSFastEnumeration or Consequences/NSFastEnumeration or Consequences/NSFastEnumeration_or_Consequences.h
@@ -0,0 +1,13 @@
+//
+//  NSFastEnumeration_or_Consequences.h
+//  NSFastEnumeration or Consequences
+//
+//  Created by Rob Rix on 11/12/2013.
+//  Copyright (c) 2013 Rob Rix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSFastEnumeration_or_Consequences : NSObject
+
+@end
